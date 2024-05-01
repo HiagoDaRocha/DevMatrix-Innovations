36ec984710a0477b4a49db2d83e6a5105473a2f2
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-05-01 16:01:46 -0300

    Mudei a lógica de redirecionar quando o login foi bem sucedido e quando a senha estiver incorreta
    E no if de verificar se o formulario foi submetido a POST, coloquei um else para permanecer na mesma página caso o formulario não for submetido a POST

--- public/php/index.php
+++ public/php/index.php
@@ -2,7 +2,7 @@
 session_start(); // Inicia a sessão
 
 // Conexão com o banco de dados
-require 'config.php'; // Certifique-se de incluir o arquivo de configuração com a conexão com o banco de dados
+require 'config.php'; // Inclusão com arquivo de configuração com a conexão com o banco de dados
 
 // Verifica se o formulário foi submetido
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -15,29 +15,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql->bindValue(':usuario', $usuario);
     $sql->execute();
 
-    // Verifica se o usuário foi encontrado
+    $login_sucesso = false;
+
     if ($sql->rowCount() === 1) {
         // Recupera os dados do usuário
         $usuario = $sql->fetch(PDO::FETCH_ASSOC);
-
+    
         // Verifica se a senha está correta
         if (password_verify($senha, $usuario['senha'])) {
             // Senha correta - Inicia a sessão do usuário
             $_SESSION['usuario'] = $usuario;
+            $login_sucesso = true;
+        }
 
-            // Redireciona para a página inicial após o login bem-sucedido
+        if ($login_sucesso) {
+            // Login bem-sucedido - Redireciona para a página inicial
             header("Location: php_testes/index.php");
             exit;
         } else {
-            // Senha incorreta - Exibe uma alert de erro e exibe uma alert de erro e redireciona para a página inicial
-            echo "<script>alert('Senha incorreta. Por favor, tente novamente.'); window.location.href = '../../public/index.html';</script>";
-
+             // Senha incorreta - Exibe uma mensagem de erro
+            header("Location: php_testes/teste.php?error=incorrect_password");
+            exit;
         }
-    } else {
+    }else {
         // Usuário não encontrado - Exibe uma mensagem de erro
         header("Location: php_testes/teste.php?error=user_not_found");
-
-
+        exit;
     }
+    
+    
+} else{
+
+    // Caso o método não for post irá permanecer na mesma página
+    header("Location: ../index.html");
+    exit;
+
 }
+
 ?>
