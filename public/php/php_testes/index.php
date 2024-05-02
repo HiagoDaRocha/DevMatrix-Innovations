0c5b6d6832961b0062b29f92544d6b2ba928bcf7
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-05-02 01:11:39 -0300

    Agora mudei o if, se a sessão estiver vazia, não entrará no site e permanecerá no index.php(login).
    E criei um html com um link para sair da sessão.

--- public/php/php_testes/index.php
+++ public/php/php_testes/index.php
@@ -1,9 +1,29 @@
 <?php
 session_start(); // Inicia a sessão
 
-// Verifica se a variável de sessão está definida
-if (isset($_SESSION['usuario'])) {
-    // A sessão está definida, então você pode acessar os dados da sessão
-    echo "Bem-vindo, " . $_SESSION['usuario']['nome']; // Supondo que 'nome' seja uma chave no array de dados do usuário
-} 
+//Verifica se a sessão esta vazia
+if (empty($_SESSION)) {
+    header("Location: ../../index.php");
+    exit;
+}
+
+// A sessão está definida, então você pode acessar os dados da sessão
+echo "Bem-vindo, " . $_SESSION['usuario']['nome']; // Supondo que 'nome' seja uma chave no array de dados do usuário
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<a href='./../logout.php'>Sair da conta</a>";
+    ?>
+    
+    
+</body>
+</html>
