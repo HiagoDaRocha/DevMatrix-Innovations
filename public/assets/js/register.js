9e6940c9512831e7f9b27765285d82d9500c579b
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-05-03 03:22:39 -0300

    Tirei a função go pois não irei mais utilizar ela

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -1,5 +1,14 @@
-/*
-function go() {
-  window.location.href = "../home.html";
-}
-*/
+document.addEventListener("DOMContentLoaded", function () {
+  const togglePassword = document.querySelector("#togglePassword"); // Seleciona o botão de alternar senha
+  const password = document.querySelector("#password-register"); // Seleciona o campo de senha
+
+  togglePassword.addEventListener("click", function () {
+    // Adiciona um ouvinte de evento para o clique no botão de alternar senha
+    const type =
+      password.getAttribute("type") === "password" ? "text" : "password"; // Obtém o tipo atual do campo de senha e alterna entre 'password' e 'text'
+    password.setAttribute("type", type); // Define o novo tipo de campo de senha
+
+    this.classList.toggle("bi-eye"); // Alterna a classe 'bi-eye' para alterar o ícone do botão de alternar senha
+    this.classList.toggle("bi-eye-slash"); // Alterna a classe 'bi-eye-slash' para alterar o ícone do botão de alternar senha
+  });
+});
