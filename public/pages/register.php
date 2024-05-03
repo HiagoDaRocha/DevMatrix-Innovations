57e955fa09e6fdf5b298a1d3da3d137b8a8ae543
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-05-03 03:27:51 -0300

    Acrescentei o bootstrap e o olhinho de ver e esconder senhas.

--- public/pages/register.php
+++ public/pages/register.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar</title>
     <link rel="stylesheet" href="../assets/css/register.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
 
 </head>
 
@@ -15,63 +16,66 @@
 
         <h1>Bem vindo ao DevMatrix Innovations</h1>
 
-       
+
     </header>
 
-        <div class="register-box">
-            <h2>Formulário de cadastro</h2>
-            <form method="post" action="../php/register.php" enctype="multipart/form-data">
-                <label for="name-complete">Nome completo:</label>
-                <input type="text" name="name-complete" id="name-complete" placeholder="Digite seu nome completo"  required>
-                
-                <label for="login">Login:</label>
-                <input type="text" name="login" id="login" placeholder="Digite um login" required><br>
-               
-                <label for="telephone">Telefone:</label>
-                <input type="tel" name="telephone" id="telephone" placeholder="Digite seu telefone" required><br>
-               
-                <label for="cpf">CPF:</label>
-                <input type="number" name="cpf" id="cpf" placeholder="Digite seu CPF" required><br>
-          
+    <div class="register-box">
+        <h2>Formulário de cadastro</h2>
+        <form method="post" action="../php/register.php" enctype="multipart/form-data">
+            <label for="name-complete">Nome completo:</label>
+            <input type="text" name="name-complete" id="name-complete" placeholder="Digite seu nome completo" required>
+
+            <label for="login">Login:</label>
+            <input type="text" name="login" id="login" placeholder="Digite um login" required><br>
+
+            <label for="telephone">Telefone:</label>
+            <input type="tel" name="telephone" id="telephone" placeholder="Digite seu telefone" required><br>
+
+            <label for="cpf">CPF:</label>
+            <input type="number" name="cpf" id="cpf" placeholder="Digite seu CPF" required><br>
+
+            <div class="password-container">
                 <label for="password-register">Senha:</label>
                 <input type="password" name="password-register" id="password-register" placeholder="Digite uma senha" required><br>
-           
-                <label for="email">E-mail:</label>
-                <input type="email" name="email" id="email" placeholder="Digite seu e-mail" required>
-                
-                <label for="birthday">Data de nascimento:</label>
-                <input type="date" name="birthday" id="birthday" required><br>
+                <i class="bi bi-eye-slash" id="togglePassword"></i>
+            </div>
 
-                <label for="file">Curriculo ou <br>portifolio:</label>
-                <input type="file" name="file" id="file" required><br><br>
+            <label for="email">E-mail:</label>
+            <input type="email" name="email" id="email" placeholder="Digite seu e-mail" required>
 
-                <label for="option">Cargo:</label>
-                <select name="linguagem" required="required" id="option">
-                    <option value="">Escolha uma linguagem ou serviço</option>
-                    <option value="front">Front-end</option>
-                    <option value="back">Back-end</option>
-                    <option value="full">Full-stack</option>
-                    <option value="csharp">C#</option>
-                    <option value="php">PHP</option>
-                    <option value="java">Java</option>
-                    <option value="javascript">JavaScript</option>
-                    <option value="cpp">C++</option>
-                    <option value="html">HTML</option>
-                    <option value="css">CSS</option>
-                    <option value="th">Técnico de hardware</option>
-                    <option value="ts">Técnico de software</option>
-                </select>
+            <label for="birthday">Data de nascimento:</label>
+            <input type="date" name="birthday" id="birthday" required><br>
 
-                <p style="text-align: center;">Fale mais sobre você:</p>
-                <textarea name="description" id="description" placeholder="Escreva sobre você, linguagens que você já trabalhou e qual você se considera melhor" cols="30" rows="10" required></textarea>
+            <label for="file">Curriculo ou <br>portifolio:</label>
+            <input type="file" name="file" id="file" required><br><br>
 
-                <input type="submit" id="register">Cadastrar</button>
+            <label for="option">Cargo:</label>
+            <select name="linguagem" required="required" id="option">
+                <option value="">Escolha uma linguagem ou serviço</option>
+                <option value="front">Front-end</option>
+                <option value="back">Back-end</option>
+                <option value="full">Full-stack</option>
+                <option value="csharp">C#</option>
+                <option value="php">PHP</option>
+                <option value="java">Java</option>
+                <option value="javascript">JavaScript</option>
+                <option value="cpp">C++</option>
+                <option value="html">HTML</option>
+                <option value="css">CSS</option>
+                <option value="th">Técnico de hardware</option>
+                <option value="ts">Técnico de software</option>
+            </select>
+
+            <p style="text-align: center;">Fale mais sobre você:</p>
+            <textarea name="description" id="description" placeholder="Escreva sobre você, linguagens que você já trabalhou e qual você se considera melhor" cols="30" rows="10" required></textarea>
+
+            <input type="submit" id="register">Cadastrar</button>
 
 
-            </form>
+        </form>
 
 
-        </div>
+    </div>
 
 
     <footer>
