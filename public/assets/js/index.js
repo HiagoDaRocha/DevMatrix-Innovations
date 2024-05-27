c921ebc1f8d74eb1723cae1b2d748b3e4d18cac4
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-05-27 03:25:23 -0300

    Fiz um alert que funciona quando clica na tag <p>esqueceu a senha?</p>, que serve para trocar senha do usuário quando ele esquecer.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -18,5 +18,56 @@ function register() {
   window.location.href = "./pages/register.php";
 }
 
+document.getElementById('forgotPasswordLink').addEventListener('click', function() {
+Swal.mixin({
+  input: 'text',
+  confirmButtonText: 'Next &rarr;',
+  showCancelButton: true,
+  progressSteps: ['1', '2'],
+}).queue([
+  {
+    title: 'Redefinir Senha',
+    text: 'Digite seu usuário',
+    inputPlaceholder: 'Usuário',
+    inputValidator: (value) => {
+      if (!value) {
+        return 'Você precisa digitar seu usuário!';
+      }
+    },
+  },
+  {
+    title: 'Redefinir Senha',
+    text: 'Digite sua nova senha',
+    inputPlaceholder: 'Nova Senha',
+    inputValidator: (value) => {
+      if (!value) {
+        return 'Você precisa digitar uma nova senha!';
+      }
+    },
+  },
+]).then((result) => {
+  if (result.value) {
+    const username = result.value[0];
+    const newPassword = result.value[1];
+    // Criei um formulário temporário para enviar a nova senha ao backend
+    var form = document.createElement('form');
+    form.method = 'POST';
+    form.action = 'php/password.php';
 
+    var usernameInput = document.createElement('input');
+    usernameInput.type = 'hidden';
+    usernameInput.name = 'username';
+    usernameInput.value = username;
+    form.appendChild(usernameInput);
 
+    var passwordInput = document.createElement('input');
+    passwordInput.type = 'hidden';
+    passwordInput.name = 'newPassword';
+    passwordInput.value = newPassword;
+    form.appendChild(passwordInput);
+
+    document.body.appendChild(form);
+    form.submit();
+  }
+});
+});
