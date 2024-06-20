e8cc0f7190611e32cb39418fda9173e3b60b899e
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-06-20 03:24:40 -0300

    Fiz um sweet alert para quando deletar algum usuário.

--- public/assets/js/usuarios.js
+++ public/assets/js/usuarios.js
@@ -2,3 +2,25 @@ function toggleMenu() {
     var x = document.getElementById("myTopnav");
     x.classList.toggle("responsive");
 }
+
+function deletar(event, url){
+
+    event.preventDefault(); // Previne o comportamento padrão do link
+
+    Swal.fire({
+        title: "Você tem certeza que quer excluir o usuário?",
+        text: "Você não poderá reverter isso!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Deletar",
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+            window.location.href = url;
+
+        }
+      });
+}
