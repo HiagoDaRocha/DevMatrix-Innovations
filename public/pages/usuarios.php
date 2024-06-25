853fec07ab5689e05d12d84f8034348e092b6e56
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-06-25 01:56:37 -0300

    Coloquei os códigos necessários para funcionar, a função de mostrar a imagem e retirei alguns códigos de php e coloquei no php/usuarios.php para deixar separado e mais organizado.

--- public/pages/usuarios.php
+++ public/pages/usuarios.php
@@ -1,30 +1,6 @@
 <?php
-session_start(); // Inicia a sessão
 
-//Verifica se a sessão está vazia
-if (empty($_SESSION)) {
-    header("Location:../index.php");
-    exit;
-}
-
-// A sessão está definida, então você pode acessar os dados da sessão
-//echo "Bem-vindo, " . $_SESSION['usuario']['nome']; // Supondo que 'nome' seja uma chave no array de dados do usuário
-
-// Conexão com o banco de dados
-require '../php/config.php'; // Inclusão com arquivo de configuração com a conexão com o banco de dados
-
-// Consulta o banco de dados para verificar o nível de acesso do usuário
-$sql = $pdo->prepare("SELECT permissoes FROM usuarios WHERE login = ?");
-$sql->bindValue(1, $_SESSION['usuario']['login']); // Supondo que 'login' seja o nome do campo de login na sua tabela de usuários
-$sql->execute();
-$dados_usuario = $sql->fetch(PDO::FETCH_ASSOC);
-
-// Verifica se o usuário é administrador
-if ($dados_usuario && $dados_usuario['permissoes'] === 1) {
-    $administrador = true;
-} else {
-    $administrador = false;
-}
+require '../php/usuarios.php';
 
 ?>
 
@@ -43,33 +19,44 @@ if ($dados_usuario && $dados_usuario['permissoes'] === 1) {
 
 <body>
 
-<script src="../assets/js/usuarios.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" ></script>
+    <script src="../assets/js/usuarios.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-<header>
-    <div class="topnav" id="myTopnav"><br>
-      <h2>DevMatrix Innovations</h2><br>
-      <a href="home.php"><i class="fa fa-fw fa-home"></i> Home</a>
-      <?php if ($administrador) : ?>
-        <a class="active" href="usuarios.php"><i class="fa fa-group"></i> Usuários</a>
-        <a href="servico.php"><i class="fa fa-briefcase"></i> Serviços</a>
-      <?php endif; ?>
-      <a href="trabalho.php"><i class="fa fa-archive"></i> Chamados</a>
-      <a href="login.php"><i class="fa fa-user-circle-o"></i> Login</a>
-      <?php
-      echo "<a href='../php/logout.php'>Sair da conta</a>";
-      ?>
-      <a href="javascript:void(0);" class="icon" onclick="toggleMenu()">
-        <div class="container">
-          <div class="bar1"></div>
-          <div class="bar2"></div>
-          <div class="bar3"></div>
+    <header>
+        <div class="topnav" id="myTopnav"><br>
+            <h2>DevMatrix Innovations</h2><br>
+            <a href="home.php"><i class="fa fa-fw fa-home"></i> Home</a>
+            <?php if ($administrador) : ?>
+                <a class="active" href="usuarios.php"><i class="fa fa-group"></i> Usuários</a>
+                <a href="servico.php"><i class="fa fa-briefcase"></i> Serviços</a>
+            <?php endif; ?>
+            <a href="trabalho.php"><i class="fa fa-archive"></i> Chamados</a>
+            <a href="login.php" id="login">
+                <?php if ($imagens == 'login-de-usuario.png') : ?>
+                    <?php
+                    $caminhoCompleto = '/tcc_tecnico/public/assets/images/' . $imagens;
+                    ?>
+                    <img src="<?php echo htmlspecialchars($caminhoCompleto); ?>" id="imgPhoto2">
+                <?php else : ?>
+                    <?php
+                    $caminhoCompleto = '/tcc_tecnico/uploadsImages/' . $imagens;
+                    ?>
+                    <img src="<?php echo htmlspecialchars($caminhoCompleto); ?>" id="imgPhoto2">
+                <?php endif; ?> Login</a>
+            <?php
+            echo "<a id='logout' href='../php/logout.php'>Sair da conta</a>";
+            ?>
+            <a href="javascript:void(0);" class="icon" onclick="toggleMenu()">
+                <div class="container">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+            </a>
         </div>
-      </a>
-    </div>
-  </header>
+    </header>
 
-  <br>
+    <br>
 
     <?php if ($administrador) : ?>
         <?php
@@ -79,46 +66,46 @@ if ($dados_usuario && $dados_usuario['permissoes'] === 1) {
         $sql = "SELECT id, nome, email, telefone, permissoes FROM usuarios";
         $resultado = $pdo->query($sql);
         ?>
-<div class="table-responsive">
-        <table class="table table-dark table-borderless">
+        <div class="table-responsive">
+            <table class="table table-dark table-borderless">
 
-            <thead>
-                <tr>
-                    <th class="border border-white">ID</th>
-                    <th class="border border-white">Nome</th>
-                    <th class="border border-white">Email</th>
-                    <th class="border border-white">Telefone</th>
-                    <th class="border border-white">Nível de acesso</th>
-                    <th class="border border-white">Remover usuário</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <?php if ($resultado->rowCount() > 0) : ?>
-
-                    <?php foreach ($resultado as $linha) : ?>
-                        <tr class="border border-white">
-                            <td class="border border-white"><?php echo $linha["id"]; ?></td>
-                            <td class="border border-white"><?php echo $linha["nome"]; ?></td>
-                            <td class="border border-white"><?php echo $linha["email"]; ?></td>
-                            <td class="border border-white"><?php echo $linha["telefone"]; ?></td>
-                            <td class="border border-white"><a href='edit.php?id=<?php echo $linha["id"]; ?>'><?php echo $linha["permissoes"]; ?></a></td>
-                            <td class="border border-white">
-                                <a href='../php/delete.php?id=<?php echo $linha["id"]; ?>' onclick="deletar(event, this.href)">Excluir</a>
-
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-
-                <?php else : ?>
+                <thead>
                     <tr>
-                        <td colspan='5'>Nenhum usuário encontrado</td>
+                        <th class="border border-white">ID</th>
+                        <th class="border border-white">Nome</th>
+                        <th class="border border-white">Email</th>
+                        <th class="border border-white">Telefone</th>
+                        <th class="border border-white">Nível de acesso</th>
+                        <th class="border border-white">Remover usuário</th>
                     </tr>
-                <?php endif; ?>
+                </thead>
 
-            </tbody>
+                <tbody>
+                    <?php if ($resultado->rowCount() > 0) : ?>
 
-        </table>
+                        <?php foreach ($resultado as $linha) : ?>
+                            <tr class="border border-white">
+                                <td class="border border-white"><?php echo $linha["id"]; ?></td>
+                                <td class="border border-white"><?php echo $linha["nome"]; ?></td>
+                                <td class="border border-white"><?php echo $linha["email"]; ?></td>
+                                <td class="border border-white"><?php echo $linha["telefone"]; ?></td>
+                                <td class="border border-white"><a href='edit.php?id=<?php echo $linha["id"]; ?>'><?php echo $linha["permissoes"]; ?></a></td>
+                                <td class="border border-white">
+                                    <a href='../php/delete.php?id=<?php echo $linha["id"]; ?>' onclick="deletar(event, this.href)">Excluir</a>
+
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+
+                    <?php else : ?>
+                        <tr>
+                            <td colspan='5'>Nenhum usuário encontrado</td>
+                        </tr>
+                    <?php endif; ?>
+
+                </tbody>
+
+            </table>
         </div>
 
     <?php endif; ?>
