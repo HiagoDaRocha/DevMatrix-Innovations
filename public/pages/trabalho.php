d969d24c1465e1a867561a7b0ca93f090cdc6c7c
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-06-25 01:56:19 -0300

    Coloquei os códigos necessários para funcionar, a função de mostrar a imagem e retirei alguns códigos de php e coloquei no php/trabalho.php para deixar separado e mais organizado.

--- public/pages/trabalho.php
+++ public/pages/trabalho.php
@@ -1,81 +1,6 @@
 <?php
-session_start(); // Inicia a sessão
 
-//Verifica se a sessão está vazia
-if (empty($_SESSION['usuario'])) {
-  header("Location:../index.php");
-  exit;
-}
-
-// Conexão com o banco de dados
-require '../php/config.php'; // Inclusão com arquivo de configuração com a conexão com o banco de dados
-
-// Consulta o banco de dados para verificar o nível de acesso do usuário
-$sql = $pdo->prepare("SELECT permissoes FROM usuarios WHERE login = ?");
-$sql->bindValue(1, $_SESSION['usuario']['login']); // Supondo que 'login' seja o nome do campo de login na sua tabela de usuários
-$sql->execute();
-$dados_usuario = $sql->fetch(PDO::FETCH_ASSOC);
-
-// Verifica se o usuário é administrador
-if ($dados_usuario && $dados_usuario['permissoes'] === 1) {
-  $administrador = true;
-} else {
-  $administrador = false;
-}
-
-// Consulta o banco de dados para obter as caixas aceitas pelo usuário atual
-$stmt = $pdo->prepare("SELECT * FROM trabalho WHERE user_id = ?");
-$stmt->execute([$_SESSION['usuario']['id']]);
-$caixas_aceitas = $stmt->fetchAll();
-
-// Consulta o banco de dados para obter os trabalhos aceitos pelo usuário atual
-$stmt = $pdo->prepare("SELECT * FROM trabalho WHERE user_id = ?");
-$stmt->execute([$_SESSION['usuario']['id']]);
-$trabalhos = $stmt->fetchAll();
-
-// Processamento dos dados do formulário
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
-  $trabalho_id = $_POST['trabalho_id'];
-  $action = $_POST['action'];
-
-  if ($action == 'concluir') {
-    // Consulta o banco de dados para obter os dados do trabalho a ser concluído
-    $stmt = $pdo->prepare("SELECT * FROM trabalho WHERE id = ? AND user_id = ?");
-    $stmt->execute([$trabalho_id, $_SESSION['usuario']['id']]);
-    $trabalho = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($trabalho) {
-
-      // Deleta o trabalho da tabela 'trabalho'
-      $stmt = $pdo->prepare("DELETE FROM trabalho WHERE id = ?");
-      $stmt->execute([$trabalho_id]);
-
-      // Insere os dados do trabalho na tabela 'chamados_concluidos'
-      $stmt = $pdo->prepare("INSERT INTO chamados_concluidos (id_user, titulo, texto, data_hora_chamado_concluido) VALUES (?, ?, ?, NOW())");
-      $stmt->execute([$_SESSION['usuario']['id'], $trabalho['titulo'], $trabalho['texto']]);
-    }
-
-    // Redirecione para a página atual
-    header("Location: trabalho.php");
-    exit(); // Certifique-se de sair após o redirecionamento
-
-  } elseif ($action == 'rejeitar') {
-    // Consulta o banco de dados para obter os dados do trabalho a ser rejeitado
-    $stmt = $pdo->prepare("SELECT * FROM trabalho WHERE id = ? AND user_id = ?");
-    $stmt->execute([$trabalho_id, $_SESSION['usuario']['id']]);
-    $trabalho = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($trabalho) {
-      // Insere os dados do trabalho na tabela 'box'
-      $stmt = $pdo->prepare("INSERT INTO box (user_id, titulo, texto) VALUES (?, ?, ?)");
-      $stmt->execute([$_SESSION['usuario']['id'], $trabalho['titulo'], $trabalho['texto']]);
-
-      // Deleta o trabalho da tabela 'trabalho'
-      $stmt = $pdo->prepare("DELETE FROM trabalho WHERE id = ?");
-      $stmt->execute([$trabalho_id]);
-    }
-  }
-}
+require '../php/trabalho.php';
 
 ?>
 
@@ -104,9 +29,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
         <a href="servico.php"><i class="fa fa-briefcase"></i> Serviços</a>
       <?php endif; ?>
       <a class="active" href="trabalho.php"><i class="fa fa-archive"></i> Chamados</a>
-      <a href="login.php"><i class="fa fa-user-circle-o"></i> Login</a>
+      <a href="login.php" id="login">
+        <?php if ($imagens == 'login-de-usuario.png') : ?>
+          <?php
+          $caminhoCompleto = '/tcc_tecnico/public/assets/images/' . $imagens;
+          ?>
+          <img src="<?php echo htmlspecialchars($caminhoCompleto); ?>" id="imgPhoto2">
+        <?php else : ?>
+          <?php
+          $caminhoCompleto = '/tcc_tecnico/uploadsImages/' . $imagens;
+          ?>
+          <img src="<?php echo htmlspecialchars($caminhoCompleto); ?>" id="imgPhoto2">
+        <?php endif; ?> Login</a>
+
       <?php
-      echo "<a href='../php/logout.php'>Sair da conta</a>";
+      echo "<a id='logout' href='../php/logout.php'>Sair da conta</a>";
       ?>
       <a href="javascript:void(0);" class="icon" onclick="toggleMenu()">
         <div class="container">
@@ -157,8 +94,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
         echo "</div>";
         echo "<br>";
       }
-    } 
-  
+    }
+
     ?>
   </div>
 
