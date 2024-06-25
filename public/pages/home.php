9ac35fa3d9a23c3257696ce44cb17a2ecabd2b4e
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-06-25 01:55:35 -0300

    Coloquei os códigos necessários para funcionar, a função de mostrar a imagem e retirei alguns códigos de php e coloquei no php/home.php para deixar separado e mais organizado.

--- public/pages/home.php
+++ public/pages/home.php
@@ -1,81 +1,6 @@
-<?php
-session_start(); // Inicia a sessão
+<?php 
 
-//Verifica se a sessão está vazia
-if (empty($_SESSION['usuario'])) {
-  header("Location:../index.php");
-  exit;
-}
-
-// Verificar se a variável de sessão 'user_logged_in' não está definida
-if (!isset($_SESSION['user_logged_in'])) {
-  // Definir a variável de sessão
-  $_SESSION['user_logged_in'] = true;
-  // Exibir o alerta de boas-vindas com o nome do usuário
-  echo "<script>
-        document.addEventListener('DOMContentLoaded', function() {
-            Swal.fire({
-                title: 'Bem-vindo ao DevMatrix Innovations!',
-                text: '" . $_SESSION['usuario']['nome'] . "',
-                icon: 'success'
-            });
-        });
-    </script>";
-}
-
-// Conexão com o banco de dados
-require '../php/config.php'; // Inclusão com arquivo de configuração com a conexão com o banco de dados
-
-// Consulta o banco de dados para verificar o nível de acesso do usuário
-$sql = $pdo->prepare("SELECT permissoes FROM usuarios WHERE login = ?");
-$sql->bindValue(1, $_SESSION['usuario']['login']); // Supondo que 'login' seja o nome do campo de login na sua tabela de usuários
-$sql->execute();
-$dados_usuario = $sql->fetch(PDO::FETCH_ASSOC);
-
-// Verifica se o usuário é administrador
-if ($dados_usuario && $dados_usuario['permissoes'] === 1) {
-  $administrador = true;
-} else {
-  $administrador = false;
-}
-
-// Consulta o banco de dados para obter as caixas
-$stmt = $pdo->query("SELECT * FROM box");
-$caixas = $stmt->fetchAll();
-
-// Processamento dos dados do formulário
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
-  $box_id = $_POST['box_id'];
-  $action = $_POST['action'];
-
-  if ($action == 'aceitar') {
-    // Consulta o banco de dados para obter os dados da caixa a ser aceita
-    $stmt = $pdo->prepare("SELECT * FROM box WHERE id = ?");
-    $stmt->execute([$box_id]);
-    $caixa = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    // Verifica se a consulta foi bem-sucedida e se $caixa não está vazio
-    if ($caixa && !empty($caixa)) {
-      // Após aceitar a caixa, remova-a da tabela 'box'
-      $stmt = $pdo->prepare("DELETE FROM box WHERE id = ?");
-      $stmt->execute([$box_id]);
-
-      // Insere os dados da caixa aceita na tabela 'trabalho' do usuário correspondente
-      $stmt = $pdo->prepare("INSERT INTO trabalho (user_id, titulo, texto) VALUES (?, ?, ?)");
-      $stmt->execute([$_SESSION['usuario']['id'], $caixa['titulo'], $caixa['texto']]);
-    }
-
-    // Armazene temporariamente o estado da caixa aceita na sessão
-    $_SESSION['caixa_aceita'] = true;
-
-    // Redirecione para a página atual
-    header("Location: home.php");
-    exit(); // Certifique-se de sair após o redirecionamento
-
-  } 
-}
-
-
+require '../php/home.php';
 
 ?>
 
@@ -87,8 +12,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>DevMatrix Innovations</title>
   <link rel="stylesheet" href="../assets/css/home.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -105,9 +31,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
         <a href="servico.php"><i class="fa fa-briefcase"></i> Serviços</a>
       <?php endif; ?>
       <a href="trabalho.php"><i class="fa fa-archive"></i> Chamados</a>
-      <a href="login.php"><i class="fa fa-user-circle-o"></i> Login</a>
+      <a id="login" href="login.php">
+        <?php if ($imagens == 'login-de-usuario.png') : ?>
+          <?php
+          $caminhoCompleto = '/tcc_tecnico/public/assets/images/' . $imagens;
+          ?>
+          <img src="<?php echo htmlspecialchars($caminhoCompleto); ?>" id="imgPhoto2">
+        <?php else : ?>
+          <?php
+          $caminhoCompleto = '/tcc_tecnico/uploadsImages/' . $imagens;
+          ?>
+          <img src="<?php echo htmlspecialchars($caminhoCompleto); ?>" id="imgPhoto2">
+        <?php endif; ?> Login</a>
       <?php
-      echo "<a href='../php/logout.php'>Sair da conta</a>";
+      echo "<a id='logout' href='../php/logout.php'>Sair da conta</a>";
       ?>
       <a href="javascript:void(0);" class="icon" onclick="toggleMenu()">
         <div class="container">
@@ -146,7 +83,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
         echo "  <span>Aceitar</span>";
         echo "</button>";
 
-        echo"</div>";
+        echo "</div>";
         echo "</form>";
         echo "</div>";
         echo "<br>";
